ffb6bfbbc76ae96a723925fbe3efb3be6e962083
Asam <abbasaliaboubakar@gmail.com>
2025-05-08 10:52:56 +0100

    add swagger

--- src/utils/setupRestEndPoint.ts
+++ src/utils/setupRestEndPoint.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
 import { UserRouter } from "../routes/user.route";
 import { ArtilceRouter } from "../routes/article.route";
+import * as swaggerUi from "swagger-ui-express";
+import * as swaggerDoc from "../swagger.json";
 
 export const setupRestEndpoint = (app: Application) => {
   app.use(express.json());
   app.use(UserRouter());
   app.use(ArtilceRouter());
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 };
